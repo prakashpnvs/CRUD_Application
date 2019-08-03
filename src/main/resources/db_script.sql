0f3f7490c6e3495a4911ddbf92d2c39b8d5183dd
Prakash <prakashpnvs@nyu.edu>
2019-08-03 19:24:56 -0400

    update db script

--- src/main/resources/db_script.sql
+++ src/main/resources/db_script.sql
@@ -2,6 +2,6 @@ create database crud_db;
 CREATE USER 'cruduser'@'%' IDENTIFIED BY 'crudpass';
 GRANT ALL PRIVILEGES ON *.* TO 'cruduser'@'%';
 USE crud_db;
-CREATE TABLE products ( product_id BIGINT(19) UNSIGNED, product_type varchar(255), product_name varchar(255));
+CREATE TABLE products ( product_id BIGINT(19) UNSIGNED AUTO_INCREMENT PRIMARY KEY, product_type varchar(255), product_name varchar(255));
 INSERT INTO products (product_id, product_type, product_name) VALUES (1, 'Electronics', 'PS4');
 SELECT * FROM products;
